482edd9b61672d95a342363a1c98b41ed3284441
zhouxiaolong <zhouxiaolong@zepp.com>
2024-03-28 11:41:52 +0800

    修改页面，跳转不同bundle

--- android/app/src/main/java/com/splitjsbundle/MainActivity.kt
+++ android/app/src/main/java/com/splitjsbundle/MainActivity.kt
@@ -1,32 +1,17 @@
-package com.splitjsbundle;
+package com.splitjsbundle
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-public class MainActivity extends ReactActivity {
+class MainActivity : AppCompatActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "SplitJsBundle";
-  }
-
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
-   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        findViewById<Button>(R.id.singleBundleBtn).setOnClickListener {
+            startActivity(Intent(this, SingleBundleActivity::class.java))
+        }
+    }
 }
